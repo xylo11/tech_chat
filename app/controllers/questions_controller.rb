578b684d340ef5cbac7bdbfc1da2efc1417ab780
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-05-03 12:35:28 +0900

    質問投稿画面 newアクション

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,8 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
+  def new
+    @question = Question.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
-  resources :questions, only: [:index]
+  resources :questions, only: [:index, :new]
 end
