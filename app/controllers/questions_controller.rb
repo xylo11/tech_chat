6060a0840d4ee4f88394f8467aa81dc0f1649b95
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-05-03 12:39:22 +0900

    保存機能 createアクション

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,4 +8,14 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def create
+    Question.create(question_params)
+    redirect_to action: :index
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :content, :name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
-  resources :questions, only: [:index, :new]
+  resources :questions, only: [:index, :new, :create]
 end
